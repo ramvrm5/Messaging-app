c729fffa34d6d8e3f3ee016ae5a5f26506296ba6
ram verma <ramvrm5@gmail.com>
2024-08-23 12:49:42 +0530

    2 - webhooks added

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://star-alpaca-7.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
